beb1fa52dc76c73f3b0ce93160ba4f298bcfecf9
ja93-hue <125463202+ja93-hue@users.noreply.github.com>
2023-02-23 06:23:31 +0530

    Add files via upload

---
+++ add func.cpp
@@ -0,0 +1,16 @@
+//program to add two numbers using function
+#include<stdio.h>
+int add(int,int);
+main()
+{
+   int m,n,p;
+   printf("enter the numbers: ");
+   scanf("%d,%d",&m,&n);
+   p=add(m,n);
+   printf("sum of the numbers is %d",p);
+}
+int add(int a,int b){
+	int c;
+	c=a+b;
+	return c;
+}
